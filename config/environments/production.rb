4d24f19577be5506b27b8d9de1fe2be3a595e69a
uesteibar <uesteibar@gmail.com>
2015-07-23 17:27:49 +0200

    user email confirmation and some styling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # mail sender config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: ENV["email_username"],
+    password: ENV["email_password"],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
 end
