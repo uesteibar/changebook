fe934620b9772ed0fd01b84de1c28ab040bff72f
uesteibar <uesteibar@gmail.com>
2015-07-24 12:22:07 +0200

    user profile and account edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,21 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.assign_attributes(user_params)
+    if @user.valid?
+      @user.save
+      redirect_to action: :show
+      return
+    end
+    render :edit
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:bio)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
+  get "/" => "home#index"
+
   devise_for :users,  path: "",
                       path_names: {sign_in: "login", sign_up: "signup", sign_out: "logout"}
-
-  get "/" => "home#index"
 
   resources :users, only: [:show, :edit, :update]
 
