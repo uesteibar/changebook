a75b61c001978d0e2d52b660a80f229c25d32e5e
uesteibar <uesteibar@gmail.com>
2015-07-24 17:24:27 +0200

    modify user location in profile editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,11 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.assign_attributes(user_params)
+    debugger
     if @user.valid?
       @user.save
-      redirect_to action: :show
+      render status: 200, json: @user
+      # redirect_to user_path(@user)
       return
     end
     render :edit
@@ -24,6 +26,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:bio)
+    params.require(:user).permit(:bio, :latitude, :longitude)
   end
 end
