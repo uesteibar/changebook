12d9b5545a70890f00195be619f7824ec314b521
uesteibar <uesteibar@gmail.com>
2015-07-27 19:05:01 +0200

    style search results

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :is_same_user?, only: [:edit]
+  before_action :authorize_user, only: [:edit, :update] # TODO - cambia esto mangarran
 
   def show
     @user = User.find(params[:id])
@@ -15,16 +15,15 @@ class UsersController < ApplicationController
     @user.assign_attributes(user_params)
     if @user.valid?
       @user.save
-      # render status: 200, json: @user
       redirect_to user_path(@user)
       return
     end
+
     render :edit
   end
 
   def search
     @users = User.where("username LIKE ?", "%#{params[:term]}%")
-    render :search
   end
 
   private
