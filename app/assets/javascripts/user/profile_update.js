71bc6108f9bff2359c0b7af3810844e8e8c0aafe
uesteibar <uesteibar@gmail.com>
2015-07-28 01:27:55 +0200

    fix issues

--- app/assets/javascripts/user/profile_update.js
+++ app/assets/javascripts/user/profile_update.js
@@ -1,14 +1,11 @@
 function profile_update_initialize() {
   var location = {};
   var input = document.getElementById('gmaps-autocomplete');
-  var autocomplete = new google.maps.places.Autocomplete(input, function(location) {
-    console.log(location);
-  });
+  var autocomplete = new google.maps.places.Autocomplete(input, function(location) {});
 
   google.maps.event.addListener(autocomplete, 'place_changed', function() {
     location = autocomplete.getPlace().geometry.location;
     $('#latitude').val(location.A);
     $('#longitude').val(location.F);
-    console.log(location);
   });
 }
