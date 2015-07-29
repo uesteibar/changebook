95ccb0645d5e333179c218bd64d37317d2103ecc
uesteibar <uesteibar@gmail.com>
2015-07-29 23:24:19 +0200

    test ownership destruction

--- spec/controllers/ownerships_controller_spec.rb
+++ spec/controllers/ownerships_controller_spec.rb
@@ -9,7 +9,26 @@ RSpec.describe OwnershipsController, type: :controller do
     @book = create(:book)
   end
 
+  describe "GET #new" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :new
+      expect(response).to be_success
+      expect(response.code.to_i).to eq(200)
+    end
+
+    it "renders the index template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
+
   describe 'create a ownership' do
+    it "responds successfully with an HTTP 201 status code" do
+      post :create, ownership: {book_id: @book.id, to_give_away: true}
+      expect(response).to be_success
+      expect(response.code.to_i).to eq(201)
+    end
+
     it 'should create a ownership' do
       expect do
         post :create, ownership: {book_id: @book.id, to_give_away: true}
@@ -20,6 +39,23 @@ RSpec.describe OwnershipsController, type: :controller do
       expect do
         post :create, ownership: {book_id: nil, to_give_away: true}
       end.to change(Ownership, :count).by(0)
+    end
+  end
+
+  describe 'destroy a ownership' do
+    before(:each) do
+      @uesteibar.ownerships.create(book_id: @book.id, to_give_away: true)
+    end
+
+    it "responds successfully with an HTTP 200 status code" do
+      post :destroy, id: @uesteibar.ownerships.last.id, user_id: @uesteibar.id
+      expect(response).to redirect_to user_path(@uesteibar)
+    end
+
+    it 'should create a ownership' do
+      expect do
+        delete :destroy, id: @uesteibar.ownerships.last.id, user_id: @uesteibar.id
+      end.to change(Ownership, :count).by(-1)
     end
   end
 end
