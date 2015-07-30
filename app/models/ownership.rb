ffdd0cecce6e1d47b117f9fb7c3d4f6690ef267a
uesteibar <uesteibar@gmail.com>
2015-07-30 15:53:35 +0200

    create events when offering a book

--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -3,4 +3,17 @@ class Ownership < ActiveRecord::Base
   belongs_to :book
 
   validates_presence_of :user_id, :book_id
+
+  after_create :create_events
+
+  private
+
+  def create_events
+    user.followers.each do |follower|
+      Event.create(
+        user_id: follower.id,
+        item_urn: "offering:#{id}"
+      )
+    end
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -12,15 +12,30 @@ RSpec.describe Event, type: :model do
       @book = create(:book)
     end
 
-    it 'should create an event on every follower when a user recommends a book' do
-      expect do
-        @uesteibar.recommend(@book, "I loved it!")
-      end.to change(@alaine.events, :count).by(1)
+    context 'when recommending a book' do
+      it 'should create an event on every follower' do
+        expect do
+          @uesteibar.recommend(@book, 'I loved it!')
+        end.to change(@alaine.events, :count).by(1)
+      end
+
+      it 'should create an event with the correct URN on every follower' do
+        recommendation = @uesteibar.recommend(@book, 'I loved it!')
+        expect(@alaine.events.last.item_urn).to eq "recommendation:#{recommendation.id}"
+      end
     end
 
-    it 'should create an event with the correct URN on every follower when a user recommends a book' do
-      recommendation = @uesteibar.recommend(@book, "I loved it!")
-      expect(@alaine.events.last.item_urn).to eq "recommendation:#{recommendation.id}"
+    context 'when offering a book' do
+      it 'should create an event on every follower' do
+        expect do
+          @uesteibar.ownerships.create(book_id: @book.id, to_exchange: true)
+        end.to change(@alaine.events, :count).by(1)
+      end
+
+      it 'should create an event with the correct URN on every follower' do
+        offering = @uesteibar.ownerships.create(book_id: @book.id, to_exchange: true)
+        expect(@alaine.events.last.item_urn).to eq "offering:#{offering.id}"
+      end
     end
   end
 end
