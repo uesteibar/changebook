2e8979a6ef45687e97ed40a5c0cd843f1b4b9e15
uesteibar <uesteibar@gmail.com>
2015-07-31 17:40:56 +0200

    sort timeline events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,6 @@ class EventsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @events = current_user.events
+    @events = current_user.events_by_date
   end
 end
