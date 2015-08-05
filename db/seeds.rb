61911ccda60263d2ee356a02f7908665d485ce40
uesteibar <uesteibar@gmail.com>
2015-08-05 21:41:46 +0200

    seed users in bcn

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,30 +18,30 @@ uesteibar = User.create(
   username: "uesteibar",
   email: "uesteibar@gmail.com",
   password: "uesteibar",
-  bio: "Yey! Here I am!",
-  latitude: 43,
-  longitude: 2
+  bio: "Yey! Here I am!"
 )
 uesteibar.confirm
 
 def create_fake_user
   username = Faker::Internet.user_name
-  user = User.create(
-    username: username,
-    email: Faker::Internet.safe_email(username),
-    password: Faker::Internet.password,
-    bio: Faker::Hacker.say_something_smart,
-    latitude: Faker::Address.latitude,
-    longitude: Faker::Address.longitude,
-  )
-  user.confirm
-  3.times do
-    genre = Genre.all
-    unless user.likes_genre?(genre)
-      user.liked_genres << genre
+  unless User.find_by(username: username)
+    user = User.create(
+      username: username,
+      email: Faker::Internet.safe_email(username),
+      password: Faker::Internet.password,
+      bio: Faker::Hacker.say_something_smart,
+      latitude: Faker::Number.between(41.0, 41.6),
+      longitude: Faker::Number.between(2.0, 2.4)
+    )
+    user.confirm
+    3.times do
+      genre = Genre.all.sample
+      unless user.likes_genre?(genre)
+        user.liked_genres.create(genre: genre)
+      end
     end
+    user.save
   end
-  user.save
 end
 
 100.times do
