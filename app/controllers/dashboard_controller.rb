16dbed2989cf29cae8ec778dec6989b0d5a2e1e6
uesteibar <uesteibar@gmail.com>
2015-08-06 02:43:24 +0200

    limit dashboard events

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @events = current_user.events_by_date
+    @events = current_user.events_by_date.limit(20)
     @system_recommendations = BookRecommendations.new.generate(user: current_user)
   end
 end
